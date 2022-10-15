9ad704ee20f26c2dfb1c8f51af293e1a5fb61c23
Revanthsaiarcot <112834630+Revanthsaiarcot@users.noreply.github.com>
2022-10-15 08:43:47 +0530

    Add files via upload

---
+++ box.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int length;
+int breadth;
+
+int main(){
+    int length, breadth;
+    cout << "Enter the length: ";
+    cin >> length;
+    cout << "Enter the breadth: ";
+    cin >> breadth;
+    cout << "length of the box: " << length;
+    cout <<"  breadth of box: "<< breadth;
+    return 0;
+}
